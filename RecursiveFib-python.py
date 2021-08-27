285e631db22114ac0b81eaa94652987ba8a9fa42
Max Morales <42706057+mwmorale@users.noreply.github.com>
2021-08-27 04:58:30 -0700

    Update RecursiveFib-python.py

--- RecursiveFib-python.py
+++ RecursiveFib-python.py
@@ -1,3 +1,6 @@
+import time as t
+start = t.time
+
 # Recursive Fibonacci calculator -> PYTHON
 
 # Input should be an integer
@@ -5,6 +8,14 @@ def fibFunc(x):
   if( x==0 or x==1 ):
     return x
   return fibFunc(x-1)+fibFunc(x-2)
+end = t.time
+
+total = start-end
+if( total<0 ):
+  total = total*-1
+  
+# Outputting run-time performance for comparison(s) with other files in repo
+print('This specific run-time performance is ', total)
   
 
 
